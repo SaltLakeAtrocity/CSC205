d5a48f228afe3a5b98dc1283d5b3f0772294a17c
SaltLakeAtrocity <rjv2749@email.vccs.edu>
2016-07-18 15:10:31 -0400

    Add files via upload

--- Four.cpp
+++ Four.cpp
@@ -15,13 +15,15 @@
 #include "productlog.h"
 using namespace std;
 
-bool opensales(ifstream & salesf, ofstream & outfile);
-void readsales(ifstream & salesf, salesType salesdata[], productType productdata[], int salescount, int prodcount);
 bool openprod(ifstream & productf);
 void readprod(ifstream & productf, productType productdata[], int prodcount);
+
+bool opensales(ifstream & salesf, ofstream & outfile);
+void readsales(ifstream & salesf, salesType salesdata[], productType productdata[], int salescount, int prodcount, int salesnum[]);
+
 float salescalc(salesType salesdata[], productType productdata[], int salescount, int prodcount);
-void outputdata(ifstream & salesf, ifstream & productf, ofstream & outfile, salesType salesdata[], productType productdata[],
-				int salescount, int prodcount);
+float bonuscalc(salesType salesdata[], productType productdata[], int salescount, int prodcount);
+void outputdata(ofstream & outfile, salesType salesdata[], productType productdata[], int salescount, int prodcount);
 
 int main() {
 	ifstream salesf, productf;
@@ -29,23 +31,39 @@ int main() {
 	salesType salesdata[30];
 	productType productdata[50];
 	int salescount=0, prodcount=0;
+    int salesnum[];
 	
-if	(! openprod(productf)) {
-	cout << "Error opening file.  Closing program.\n";
-	return 1;
-}
+    if	(! openprod(productf)) { cout << "Error opening file.  Closing program.\n"; return 1; }
 	readprod(productf, productdata, prodcount);
-if (! opensales(salesf, outfile)) {
-	cout << "Error opening file.  Closing program.\n";
-	return 1;
-}
-	readsales(salesf, salesdata, productdata, salescount, prodcount);
-	salesdata[salescount].totalsales = salescalc(salesdata, productdata, salescount, prodcount);
-	outputdata(salesf, productf, outfile, salesdata, productdata, salescount, prodcount);
+    
+    if (! opensales(salesf, outfile)) { cout << "Error opening file.  Closing program.\n"; return 1; }
+	readsales(salesf, salesdata, productdata, salescount, prodcount, salesnum);
+        //salesearch() called from readsales()
+    
+	salesdata[salescount].totalsales == salescalc(salesdata, productdata, salescount, prodcount);
+    salesdata[salescount].bonus == bonus(salesdata, productdata, salescount, prodcount);
+	outputdata(outfile, salesdata, productdata, salescount, prodcount);
 	return 0;
 }
 //************************************************************************************
-bool opensales(ifstream & salesf, ofstream & outfile) {
+bool openprod(ifstream & productf) {
+    productf.open("products.txt");
+    if (! productf.is_open()) {
+        return false;
+    }
+    return true;
+}
+//************************************************************************************
+void readprod(ifstream & productf, productType productdata[], int prodcount) {
+    productf >> productdata[prodcount].productnum >> productdata[prodcount].price;
+    while (productf) {
+        productf >> productdata[prodcount].productnum >> productdata[prodcount].price;
+        prodcount++;
+    }
+}
+//************************************************************************************
+//************************************************************************************
+bool opensales(ifstream & salesf, ifstream & outfile) {
     salesf.open("salesFile.txt");
 	outfile.open("Outputfile.txt");
 	if (! salesf.is_open()) {
@@ -58,44 +76,40 @@ bool opensales(ifstream & salesf, ofstream & outfile) {
 	return true;
 }
 //************************************************************************************
-void readsales(ifstream & salesf, salesType salesdata[], productType productdata[], int salescount, int prodcount) {
-	int i, salesnum;
+void readsales(ifstream & salesf, salesType salesdata[], productType productdata[], int salescount, int prodcount, int salesnum[]) {
+    int j = 0;
 	while (salesf) {
-		salesf 	>> 	salesdata[salescount].firstname >> salesdata[salescount].lastname >> salesdata[salescount].id;
-		while (salesnum != -1) {
-			salesf	>>	salesnum >> salesdata[salescount].numsales;
-			for (i = 0, i < productdata[i].productnum, i++) {
-				if (salesnum==productdata[i].productnum) {
-					
-				}
-			}
+        salesf 	>> 	salesdata[salescount].firstname >> salesdata[salescount].lastname >> salesdata[salescount].id >> salesnum[j];
+		while (salesnum[j] != -1) {
+            salesf	>>	salesdata[salescount].numsales[j];
+            salesdata[salescount].itemsales[j] = salesearch(salesdata, productdata, salescount, prodcount, salesnum);
+            j++;
+            salesf  >>  salesnum[j];
 		}
-	}
+    }
 		salescount++;
 }
 //************************************************************************************
-bool openprod(ifstream & productf) {
-    productf.open("products.txt");
-	if (! productf.is_open()) {
-		return false;
-	}
-	return true;
+float salesearch(salesType salesdata[], productType productdata[], int salescount, int prodcount, int salesnum[]) {
+    int j = 0;
+    for (i = 0; i < productdata[i].productnum; i++) {
+        if (salesnum[j]==productdata[i].productnum) {
+            salesdata[salescount].itemsales[j] = productdata[i].price * salesdata[salescount].numsales;
+        }
+    }
+    return salesdata[salescount].itemsales;
 }
 //************************************************************************************
-void readprod(ifstream & productf, productType productdata[], int prodcount) {
-		productf >> productdata[prodcount].productnum >> productdata[prodcount].price;
-	while (productf) {
-		productf >> productdata[prodcount].productnum >> productdata[prodcount].price;
-		prodcount++;
-	}
-}
 //************************************************************************************
 float salescalc(salesType salesdata[], productType productdata[], int salescount, int prodcount) {
-	//salesdata[salescount].totalsales
+    
 }
 //************************************************************************************
-void outputdata(ifstream & salesf, ifstream & productf, ofstream & outfile, salesType salesdata[], productType productdata[],
-				int salescount, int prodcount) {
+float bonuscalc(salesType salesdata[], productType productdata[], int salescount, int prodcount) {
+    
+}
+//************************************************************************************
+void outputdata(ofstream & outfile, salesType salesdata[], productType productdata[], int salescount, int prodcount) {
 		
 }
 //************************************************************************************
