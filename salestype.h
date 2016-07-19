672bae73c0c61898c10defd7638d0b5fb5ac4a44
SaltLakeAtrocity <rjv2749@email.vccs.edu>
2016-07-19 01:14:35 -0400

    Add files via upload

---
+++ salestype.h
@@ -0,0 +1,21 @@
+// Robert VanCleave
+// salestype.h
+// header file for Programming_Assignment_Four.cpp
+// 
+
+#include <string>
+using namespace std;
+
+struct salesType
+{
+	string	firstname;
+	string	lastname;
+	int		id;
+	float	totalsales;
+	float	bonus;
+  
+};
+
+
+
+
