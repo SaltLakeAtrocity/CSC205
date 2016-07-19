97b0f23e7da31d30caadeb9691db12baa1042727
SaltLakeAtrocity <rjv2749@email.vccs.edu>
2016-07-19 01:14:27 -0400

    Add files via upload

---
+++ productlog.h
@@ -0,0 +1,12 @@
+// Robert VanCleave
+// productlog.h
+// header file for Programming_Assignment_Four.cpp
+// 
+
+using namespace std;
+
+struct productType
+{
+	float	productnum;
+	float	price;
+};
